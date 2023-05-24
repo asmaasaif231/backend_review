d43ccbc26eb7f5468bbd2ebfa7e1db1a92edc52d
asmaasaif231 <124551507+asmaasaif231@users.noreply.github.com>
2023-05-24 11:31:04 +0400

    Add files via upload

--- asmaa_16may/review_backend/src/main/java/com/example/review_backend/SpringBootJdbcController.java
+++ asmaa_16may/review_backend/src/main/java/com/example/review_backend/SpringBootJdbcController.java
@@ -1,11 +1,10 @@
 package com.example.review_backend;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,11 +35,18 @@ public String StoreData( @RequestBody String request) throws IOException{
     System.out.println(reviewComment);
     // replace with your table query
   
-
-
     jdbc.execute("INSERT INTO `sys`.`review` (`comment`, `rate`, `name`) VALUES ('"+reviewComment+"','"+reviewRate+"','"+username+"');");
 
     return"data inserted Successfully";  
+
+}
+    @RequestMapping(path = "/getItems", method = RequestMethod.GET) 
+    
+    public List<Item> getItems() throws IOException{
+        List<Item> items = jdbc.query("SELECT * FROM `sys`.`course`", new ItemMapper());
+        return items;
+}
+
 }
 
     // @Autowired
@@ -51,7 +57,7 @@ public String StoreData( @RequestBody String request) throws IOException{
     //     jdbc.execute("INSERT INTO `sys`.`course` (`idCourse`, `courseName`, `courseDesc`, `courseDuration`, `cStartDate`, `cEndDate`, `cLocation`, `cLevel`) VALUES ('100', 'Best course', 'Best course', '1 MONTH', '09-JUL-2023', '09-AUG-2023', 'ONLNE', 'Entry Level');");
     //     return "Insertion successful";
     // }
-}
+
 
 
 // http://localhost:2400/
